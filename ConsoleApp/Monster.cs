6e6ef2eed62ad5be2ba67a399b16f4444d8ee5b6
rebecca <rebeccazakarian05@gmail.com>
2026-06-29 15:56:10 -0700

    heal and dead

--- ConsoleApp/Monster.cs
+++ ConsoleApp/Monster.cs
@@ -1,37 +1,50 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Resources;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ConsoleApp;
 
-public class Monster(int hp, int attack, int defense)
+public class Monster(int hp, int attack, int defense, string name)
 {
+    public string Name { get; } = name;
     public int Hp { get; private set; } = hp;
     public int Attack { get; } = attack;
     private int attackCount = 0;
     public int Defense { get; } = defense;
     public bool Determination { get; set; }
     public int DefendCharge { get; set; }
-
+    public bool HealCharge { get; set; } // i want healing to recover 30 health points (HPs)
+    public bool IsDead { get; set; }
     public void AttackMonster(Monster opponent)
     {
         attackCount++;
-        if (Hp > 0)
+        if (Hp > 0 && opponent.IsDead == false)
             opponent.TakeDamage(Attack);
         else
             return;
         if (attackCount % 3 == 0)
             DefendCharge++;
+        else if (attackCount % 5 == 0)
+            HealCharge = true;
     } 
 
     private void TakeDamage(int attack)
     {
         int newHp = Hp - (attack - (Defense + DefendCharge));
         {
-            if (Determination == true && Hp > 1 && newHp < 1)
-                Hp = 1;
-            else
+            if (Determination == true && Hp > 1 && newHp < 1 && HealCharge == true)
+                Hp = 31;
+            else if (Determination == true && Hp > 1 && newHp < 1 && HealCharge == false)
                 Hp = newHp;
+            else if (newHp > 0)
+                Hp = newHp;
+            else if (newHp <= 0)
+                Hp = newHp;
+                IsDead = true;
+                Console.WriteLine($"{Name} is now dead");
         }
         DefendCharge = 0;
+        HealCharge = false;
     }   
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,26 +6,21 @@ class Program
     {
         Console.WriteLine("Battle Start!");
 
-        Monster monster1 = new Monster(30, 10, 3);
+        Monster monster1 = new Monster(50, 10, 50, "monster1");
         monster1.Determination = true;
-        Monster monster2 = new Monster(20, 5, 3);
+        Monster monster2 = new Monster(50, 5, 50, "monster2");
 
         Console.WriteLine("monster1 stats:");
         Console.WriteLine($"HP: {monster1.Hp}");
         Console.WriteLine($"Attack: {monster1.Attack}");
         Console.WriteLine($"Defense: {monster1.Defense}");
 
-        monster1.AttackMonster(monster2);
-        monster1.AttackMonster(monster2);
-        monster1.AttackMonster(monster2);
-
-        Console.WriteLine("monster1 attacked monster2 3 times.");
-        Console.WriteLine($"monster2 HP is now {monster2.Hp}");
-
-        monster2.AttackMonster(monster1);
-        monster2.AttackMonster(monster1);
-
-        Console.WriteLine("monster 2 attacked monster 1.");
-        Console.WriteLine($"Montster 1 is now at {monster1.Hp}.");
+    while (monster1.IsDead == false && monster2.IsDead == false)
+        {
+            monster1.AttackMonster(monster2);
+            if (monster2.IsDead == true) break;
+            monster2.AttackMonster(monster1);
+            if (monster1.IsDead == true) break;
+        }
     }
 }
